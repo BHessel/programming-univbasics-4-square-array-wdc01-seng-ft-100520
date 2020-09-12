ba3e695ce9d00f75ff1a380f1ef9ce5ab96209f7
Benjamin Hessel <hesselfitness@gmail.com>
2020-09-12 23:29:20 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -9,3 +9,7 @@ end
 
 
 
+# following is just thoughts for the tech coach
+  # should line 3 be new_array = [] ?
+  # in line 4 do I need the "do"? Is it ok with or without?
+  # I know ** is the exponent operator, 
